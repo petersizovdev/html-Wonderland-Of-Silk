3269d719ce4e8e63a90dad3a098cfaf29b2f39c6
pitersizovdev <pitersizov@gmail.com>
2024-04-26 17:14:58 +0300

    0.8.0

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('open-modal').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'block';
+  });
+  
+  document.getElementsByClassName('close')[0].addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+  });
+  
+  window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('modal')) {
+      document.getElementById('modal').style.display = 'none';
+    }
+  });
