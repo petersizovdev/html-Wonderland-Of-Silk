1549ab36a1e08a84ec485efda1fa2c68b1becfad
pitersizovdev <pitersizov@gmail.com>
2024-04-27 01:16:09 +0300

    b-

--- script.js
+++ script.js
@@ -14,15 +14,3 @@ window.addEventListener("click", function (event) {
   }
 });
 
-if (window.devicePixelRatio !== 1) {
-  var dpt = window.devicePixelRatio;
-  var widthM = window.screen.width * dpt;
-  var widthH = window.screen.height * dpt;
-  document.write(
-    '<meta name="viewport" content="width=' +
-      widthM +
-      ", height=" +
-      widthH +
-      '">'
-  );
-}
